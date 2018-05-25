09afb85c6823b0e2fd2a5e51c72d2ab321f1df1d
tai <tai.ntt>
2018-05-25 16:08:58 +0700

    update API

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ var fs = require("fs");
 
 var server = http.createServer();
 
-server.on("request", (req, res) => {
+// server.on("request", (req, res) => {
+
+// });
+
+
+server.get('/getSanPham', function (req,res) {
     var data = fs.readFileSync("./du_lieu/san_pham.json");
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader('content-type', 'application/json');
 	console.log(data);
     res.end(data);
-});
+ });
+
+
+ 
 
 server.listen(process.env.PORT || 5000) ;
