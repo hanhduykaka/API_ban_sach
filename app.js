e1fea0cbf2947b34d0da2c0e416bc4a1fe092223
tai <tai.ntt>
2018-05-25 16:53:05 +0700

    update api get and post

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-var http = require("http");
+
 var fs = require("fs");
 
-var server = http.createServer();
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json())
 
-// server.on("request", (req, res) => {
-
-// });
-
-
-server.get('/getSanPham', function (req,res) {
+app.get('/getSanPham', function (req, res) {
     var data = fs.readFileSync("./du_lieu/san_pham.json");
+
+    res.end(data);
+});
+
+app.post('/postUserName', function (req, res) {
+    var data = fs.readFileSync("./du_lieu/nguoi_dung.json");
+
+    if (req.body.token == "1234567890")
+        res.end(data);
+    else {
+        res.end("please try another token");
+    }
+});
+
+app.get('*', function (req, res) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader('content-type', 'application/json');
-	console.log(data);
-    res.end(data);
- });
+    res.end("Please try again with another source api");
+});
 
 
- 
 
-server.listen(process.env.PORT || 5000) ;
+app.listen(process.env.PORT || 5000);
