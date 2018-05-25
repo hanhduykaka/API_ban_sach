ad30e873ed40aa737f9d3cd0695751ca56704397
tai <tai.ntt>
2018-05-25 15:52:28 +0700

    done

---
+++ app.js
@@ -0,0 +1,14 @@
+var http = require("http");
+var fs = require("fs");
+
+var server = http.createServer();
+
+server.on("request", (req, res) => {
+    var data = fs.readFileSync("./du_lieu/san_pham.json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('content-type', 'application/json');
+	console.log(data);
+    res.end(data);
+});
+
+server.listen(process.env.PORT || 5000) ;
